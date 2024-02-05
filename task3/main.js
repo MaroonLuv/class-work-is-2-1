c922b117fddc851851feeecd3d29e76b13d22884
MaroonLuv <sky_my@list.ru>
2024-02-05 13:49:12 +0300

    Выполнили работу с объектами

--- task3/main.js
+++ task3/main.js
@@ -1,3 +1,27 @@
-const students = ["Андрей", "Иван", "Стас"];
-const student = students[1];
-console.log(student);
+
+
+
+
+
+let college = [
+    {name: "Tom", age: 23, group: "ИС-2-2"},
+    {name: "Bob", age: 15, group: "ИС-2-1"},
+    {name: "Stan", age: 23, group: "ИС-2-2"},
+    {name: "Harry", age: 17, group: "ИС-4-1"}
+];
+
+let count = 2;
+let studentAge = college[count].age;
+let studentName = college[count].name;
+
+if (studentAge >= 18) {
+    console.log(`Студент ${studentName} совершеннолетний. Ему ${studentAge} лет`)
+} else if (studentAge < 18) {
+    console.log(`Студент ${studentName} не совершеннолетний. Ему ${studentAge} лет`)
+};
+
+
+
+
+
+
