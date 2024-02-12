ca67c36caa9bd5a37e7e798c9c53f2cbeadc3cb5
MaroonLuv <sky_my@list.ru>
2024-02-12 10:16:54 +0300

    Выполнили работу с циклами

---
+++ task4/main.js
@@ -0,0 +1,19 @@
+// const student = ["Tom", "bob", "Harry", "Ron", "Stan"];
+
+// for (let i = 0; i < student.length; i++) {
+//     let result = student[i];
+//     console.log(result);
+// };
+
+
+let people = [ ["Tom", 18], ["Bob", 17], ["Stan", 36]];
+
+for (let i = 0; i < people.length; i++) {
+    for (let j = 0; j < people[i].length; j++) {
+        let result = people[i][j];
+        console.log(result);
+    };
+    console.log("-------------")
+};
+
+
